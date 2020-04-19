e806ca1508eef4990344605ef738a9ddeea13368
Ryubiz0601 <63944494+Ryubiz0601@users.noreply.github.com>
2020-04-19 22:06:26 +0900

    kintai

---
+++ kintai.js
@@ -0,0 +1,51 @@
+{\rtf1\ansi\ansicpg932\cocoartf2512
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\fnil\fcharset128 HiraginoSans-W3;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 unction kintai(btnNo)\{\
+  if (btnNo == 1)\{\
+    link = "
+\f1 \'8e\'64\'8e\'96\'82\'cc\'8a\'4a\'8e\'6e
+\f0 ";\
+    href = "https://azalea.tokimane.jp/castchat/job?String str=/" + encodeURIComponent('
+\f1 \'8e\'64\'8e\'96\'82\'f0\'8e\'6e\'82\'df\'82\'dc\'82\'b5\'82\'bd
+\f0 ');\
+  \}else\
+  if (btnNo == 2)\{\
+    link = "
+\f1 \'8b\'78\'8c\'65
+\f0 ";\
+    href = "https://azalea.tokimane.jp/castchat/job?String str=/" + encodeURIComponent('
+\f1 \'8b\'78\'8c\'65\'82\'b5\'82\'dc\'82\'b7
+\f0 ');\
+  \}else\
+  if (btnNo == 3)\{\
+    link = "
+\f1 \'8b\'78\'8c\'65\'82\'a9\'82\'e7\'82\'cc\'96\'df\'82\'e8
+\f0 ";\
+    href = "https://azalea.tokimane.jp/castchat/job?String str=/" + encodeURIComponent('
+\f1 \'8b\'78\'8c\'65\'8f\'49\'82\'ed\'82\'e8\'82\'dc\'82\'b5\'82\'bd
+\f0 ');\
+  \}else\
+  if (btnNo == 4)\{\
+    link = "
+\f1 \'8e\'64\'8e\'96\'82\'cc\'8f\'49\'97\'b9
+\f0 ";\
+    href = "https://azalea.tokimane.jp/castchat/job?String str=/" + encodeURIComponent('
+\f1 \'8e\'64\'8e\'96\'82\'aa\'8f\'49\'82\'ed\'82\'e8\'82\'dc\'82\'b5\'82\'bd
+\f0 ');\
+  \}\
+\
+  ret = confirm(link + "
+\f1 \'82\'f0\'83\'67\'83\'4c\'83\'7d\'83\'6c\'82\'c9\'8b\'4c\'98\'5e\'82\'b5\'82\'dc\'82\'b7\'81\'42\'8b\'58\'82\'b5\'82\'a2\'82\'c5\'82\'b7\'82\'a9\'81\'48
+\f0 ");\
+  if (ret == true)\{\
+    location.href = href;\
+  \}\
+\}\
+\pard\pardeftab720\partightenfactor0
+\cf0 \
+}
